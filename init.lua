623247dff201ba3a3a38955450ed4d32e5f58629
Jamie Drinkell <94254558+M0JXD@users.noreply.github.com>
2025-03-14 21:27:52 +0000

    Better theme switching, remove rust debugging

--- init.lua
+++ init.lua
@@ -5,43 +5,34 @@ require('lua_repl')
 -- require('spellcheck')
 
 -- Debugger setup (on Linux anyways)- can call reset command and should auto-setup
-if not (WIN32 or OSX) then 
+if not WIN32 or OSX then 
     events.connect(events.LEXER_LOADED, function(name)
         local project_dir = io.get_project_root()
         local project_bin
 
-        if name == 'c' or name == 'cpp' then
+        if name == 'c' or name == 'cpp' and not CURSES then
             local debugger = require('debugger')
             -- Xmake hierachy
-            project_bin = io.get_project_root() .. '/build/linux/x86_64/debug/' .. io.get_project_root():match(".*/(.*)$")
-            debugger.project_commands[project_dir] = function()
-                return 'c', project_bin
-            end
+            --project_bin = io.get_project_root() .. '/build/linux/x86_64/debug/' .. io.get_project_root():match(".*/(.*)$")
+            --debugger.project_commands[project_dir] = function()
+            --    return 'c', project_bin
+            --end
             
-            -- TODO: implement CMake and actually check these paths exist before setting up the debugger
-            
-        elseif name == 'rust' then
-            local debugger = require('debugger')
-            -- Try for Rust (Cargo)
-            project_bin = io.get_project_root() .. '/target/debug/' .. io.get_project_root():match(".*/(.*)$")
-            
-            -- Need to implement/check support for Rust's rust-gdb wrapper, can use C for now
-            debugger.project_commands[project_dir] = function()
-                return 'c', project_bin
-            end
+            -- TODO: implement CMake (maybe more like Meson) and actually check these paths exist before setting up the debugger
         end
     end)
 end
 
 -- LSP
-local lsp = require('lsp')
+--local lsp = require('lsp')
 --lsp.server_commands.cpp = 'clangd'
-lsp.server_commands.rust = 'rust-analyzer'
-lsp.server_commands.dart = 'dart language-server'
+--lsp.server_commands.rust = 'rust-analyzer'
+--lsp.server_commands.dart = 'dart language-server'
 -- Use Textadept's basic autocompletion for ''ctrl+ ' in the terminal, LSP module seems unhappy with TUI Textadept
 if CURSES then
     keys['ctrl+ '] = function() textadept.editing.autocomplete('word') end
 end
+
 
 -- File Browser
 local file_browser = require('file_browser')
@@ -63,7 +54,7 @@ buffer.tab_width = 4
 lexer.detect_extensions.C = 'cpp'
 lexer.detect_extensions.H = 'cpp'
 
--- Language specific overrides
+-- Language specific overrides (currently uneeded)
 events.connect(events.LEXER_LOADED, function(name)
     if name == 'dart' then
         buffer.tab_width = 2
@@ -77,16 +68,14 @@ end)
 if not CURSES then
     events.connect(events.MODE_CHANGED, function()
         if _THEME == 'dark' then
-            -- view:set_theme('dark', {font = 'Monospace', size = 12})
-            view:set_theme('ayuesque-dark', {font = 'Noto Mono', size = 12})
+            for _, view in ipairs(_VIEWS) do view:set_theme('ayuesque-dark', {font = 'Noto Mono', size = 12}) end
+            pcall(function () ui.command_entry:set_theme('ayuesque-dark', {font = 'Noto Mono', size = 12}) end)
         else 
-            -- view:set_theme('light')
-            view:set_theme('ayuesque-light', {font = 'Noto Mono', size = 12})
+            for _, view in ipairs(_VIEWS) do view:set_theme('ayuesque-light', {font = 'Noto Mono', size = 12}) end
+            pcall(function () ui.command_entry:set_theme('ayuesque-light', {font = 'Noto Mono', size = 12}) end)
         end
     end)
     events.emit(events.MODE_CHANGED)
-else
-    view:set_theme('term')
 end
 
 -- Add a suspend menu entry for terminal
