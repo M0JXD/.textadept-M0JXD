0212c98a57153a09554822d42bf32f2cc2ea57de
M0JXD <jamiedrinkell@gmail.com>
2025-10-13 23:44:37 +0100

    Create theme manager module

---
+++ modules/theme_mgr/init.lua
@@ -0,0 +1,62 @@
+-- Copyright 2025 Jamie Drinkell. MIT License.
+
+-- This is a simple theme manager for Textadept.
+-- Allow system switching and automatic detection/application of what's best in some environments.
+-- Handy if you don't want to override the default themes to acheive this
+-- (Leaving a handy fallback for when you edit your theme and crash everything...)
+
+local M = {}
+M.light_theme = 'light'
+M.dark_theme = 'dark'
+M.term_theme = 'term'
+M.term_fallback_theme = 'term'
+M.font_type = 'Monospace'
+M.font_size = 12
+M.win32_default_font = true
+M.curses_apply_font = false
+
+-- Windows fonts are not always available, so override to the default type
+-- TODO: Check this works
+if WIN32 then
+    events.connect(events.INITIALIZED, function()
+        if M.win32_default_font then M.font_type = 'Monospace' end
+    end)
+end
+
+-- GUI Themeing
+-- TODO: GTK2 version doesn't detect system colours?
+if not CURSES then
+    events.connect(events.VIEW_NEW, function()
+        if _THEME == 'dark' then
+            view:set_theme(M.dark_theme, {font = M.font_type, size = M.font_size})
+        else
+            view:set_theme(M.light_theme, {font = M.font_type, size = M.font_size})
+        end
+    end)
+
+    events.connect(events.MODE_CHANGED, function()
+        if _THEME == 'dark' then
+            for _, view in ipairs(_VIEWS) do view:set_theme(M.dark_theme, {font = M.font_type, size = M.font_size}) end
+            pcall(function () ui.command_entry:set_theme(M.dark_theme, {font = M.font_type, size = M.font_size}) end)
+        else
+            for _, view in ipairs(_VIEWS) do view:set_theme(M.light_theme, {font = M.font_type, size = M.font_size}) end
+            pcall(function () ui.command_entry:set_theme(M.light_theme, {font = M.font_type, size = M.font_size}) end)
+        end
+    end)
+    events.emit(events.MODE_CHANGED)
+
+-- CURSES Theming
+elseif not WIN32 then
+    -- GNOME Terminal, Tilix, Konsole, XFCE, LXDE etc. all report xterm-256color
+    local terminal = os.getenv("TERM")
+    events.connect(events.INITIALIZED, function()
+        if (terminal == 'xterm-256color') or (terminal == 'alacritty') then
+            ui.statusbar_text = M.term_theme
+            view:set_theme(M.term_theme)
+        elseif (terminal == 'xterm') or (terminal == 'linux') then
+            view:set_theme(M.term_fallback_theme)
+        end
+    end)
+end
+
+return M
